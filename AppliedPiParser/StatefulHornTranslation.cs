9b48b0d278b43bd397e80b64d93b6253f27aca1b
Nick Curran <6274048+n-crrn@users.noreply.github.com>
2022-03-10 22:52:58 +1000

    Start of the implementation of the Applied Pi to Stateful Horn translation. Currently translating free names, constants, constructors and destructors. Tests added and passing.

---
+++ AppliedPiParser/StatefulHornTranslation.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using StatefulHorn;
+
+namespace AppliedPi;
+
+/// <summary>
+/// Represents a completed translation from an Applied Pi source to a series of Stateful Horn
+/// clauses. Not every Applied Pi source will result in a valid set of clauses. Therefore,
+/// this class allows for holding a partial translation in a way that, for instance, 
+/// StatefulHorn.QueryEngine does not.
+/// </summary>
+public class StatefulHornTranslation
+{
+
+    public HashSet<State> InitialState { get; } = new();
+
+    public IMessage? QueryMessage { get; internal set; }
+
+    public State? QueryWhen { get; internal set; }
+
+    public HashSet<Rule> Rules { get; } = new();
+
+    #region Basic object overrides.
+
+    private static bool IMessageEquals(IMessage? msg1, IMessage? msg2) => msg1 == null ? msg2 == null : msg1.Equals(msg2);
+
+    public override bool Equals(object? other)
+    {
+        return other is StatefulHornTranslation sht &&
+            InitialState.SetEquals(sht.InitialState) &&
+            IMessageEquals(QueryMessage, sht.QueryMessage) &&
+            State.Equals(QueryWhen, sht.QueryWhen) &&
+            Rules.SetEquals(sht.Rules);
+    }
+
+    public static bool operator ==(StatefulHornTranslation sht1, StatefulHornTranslation sht2) => Equals(sht1, sht2);
+
+    public static bool operator !=(StatefulHornTranslation sht1, StatefulHornTranslation sht2) => !Equals(sht1, sht2);
+
+    public override int GetHashCode()
+    {
+        int hc = 31;
+        foreach (State s in InitialState)
+        {
+            hc = hc * 41 + s.GetHashCode();
+        }
+        foreach (Rule r in Rules)
+        {
+            hc = hc * 41 + r.GetHashCode();
+        }
+        return hc;
+    }
+
+    #endregion
+    #region Describe translation.
+
+    public void Describe(TextWriter writer)
+    {
+        writer.WriteLine("Initial state: " + string.Join(", ", InitialState));
+        writer.WriteLine($"Query: {QueryMessage}");
+        if (QueryWhen != null)
+        {
+            writer.WriteLine($"When: {QueryWhen}");
+        }
+        writer.WriteLine("Rules:");
+        foreach (Rule r in Rules)
+        {
+            writer.WriteLine("  " + r.ToString());
+        }
+    }
+
+    #endregion
+
+}
---
+++ AppliedPiTest/AppliedPiTest/TranslationTests.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using AppliedPi;
+using StatefulHorn;
+using StatefulHorn.Messages;
+
+namespace SarsaparillaTests.AppliedPiTest;
+
+[TestClass]
+public class TranslationTests
+{
+
+    private static void DoTest(StatefulHornTranslation expectedSht, Network nw)
+    {
+        StatefulHornTranslation sht = StatefulHornTranslator.Translate(nw);
+        try
+        {
+            Assert.AreEqual(expectedSht, sht, "Translation for constructors/destructors failed.");
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("--- Expected translation ---");
+            expectedSht.Describe(Console.Out);
+            Console.WriteLine("--- Generated translation ---");
+            sht.Describe(Console.Out);
+            throw;
+        }
+    }
+
+    [TestMethod]
+    public void ConstantAndFreeTests()
+    {
+        // Sample input.
+        string testSource =
+            "free A, B: host.\n" +
+            "free C: host [private].\n" +
+            "const D: host.";
+        Network nw = Network.CreateFromCode(testSource);
+
+        // Create expected output for comparison.
+        RuleFactory factory = new RuleFactory();
+        StatefulHornTranslation expectedSht = new();
+        foreach (string name in new string[] { "A", "B", "D" })
+        {
+            expectedSht.Rules.Add(factory.CreateStateConsistentRule(Event.Know(new NameMessage(name))));
+        }
+
+        // Testing and checking.
+        DoTest(expectedSht, nw);
+    }
+
+    [TestMethod]
+    public void ConstructorTests()
+    {
+        string testSource = 
+            "fun pk(skey): pkey.\n" +
+            "reduc forall x: bitstring, y: skey; decrypt(encrypt(x, y), y) = x.";
+        Network nw = Network.CreateFromCode(testSource);
+
+        // Create expected output for comparison.
+        RuleFactory factory = new();
+        StatefulHornTranslation expectedSht = new();
+        factory.RegisterPremise(Event.Know(new VariableMessage("skey")));
+        expectedSht.Rules.Add(factory.CreateStateConsistentRule(Event.Know(MessageParser.ParseMessage("pk(skey)"))));
+        factory.RegisterPremise(Event.Know(MessageParser.ParseMessage("decrypt(encrypt(x, y), y)")));
+        expectedSht.Rules.Add(factory.CreateStateConsistentRule(Event.Know(new VariableMessage("x"))));
+
+        // Testing and checking.
+        DoTest(expectedSht, nw);
+    }
+
+}
