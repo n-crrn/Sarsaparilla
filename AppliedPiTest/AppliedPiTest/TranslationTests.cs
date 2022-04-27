a62f17bca0f4e64acf4c016874a0a3fb5d77435b
Nick Curran <6274048+n-crrn@users.noreply.github.com>
2022-04-27 15:00:58 +1000

    Code clean-up in TranslationTests in preparation for adding new tests.

--- AppliedPiTest/AppliedPiTest/TranslationTests.cs
+++ AppliedPiTest/AppliedPiTest/TranslationTests.cs
@@ -33,19 +33,21 @@ process out(c, v) | in(c, v: bitstring).";
             new State("c@1@Out", new NameMessage("_Initial")),
             new State("c@2@In", new NameMessage("_Initial"))
         };
-        RuleParser rp = new();
-        HashSet<Rule> expectedRules = new()
-        {
-            rp.Parse("-[ ]-> k(c[])"),
-            rp.Parse("-[ (c@1@Out(_Initial[]), a0) ]-> <a0: c@1@Out(_Write(v))>"),
-            rp.Parse("-[ (c@2@In(_Initial[]), a0) ]-> <a0: c@2@In(_Waiting[])>"),
-            rp.Parse("-[ (c@1@Out(_Initial[]), a0), (c@1@Out(_Write(v)), a1), (c@2@In(_Waiting[]), b0) : " +
-                     "{ a0 <@ a1 } ]-> <a1: c@1@Out(_Shut[])>, <b0: c@2@In(_Read(v))>"),
-            rp.Parse("k(c[])(a0) -[ (c@1@Out(_Write(_vLatest)), a0) ]-> k(_vLatest)"),
-            rp.Parse("-[ (c@2@In(_Initial[]), a0), (c@2@In(_Waiting[]), a1), (c@2@In(_Read(_v0)), a2) : { a0 <@ a1, a1 <@ a2 } ]-> <a2: c@2@In(_Shut[])>")
-        };
+        HashSet<Rule> expectedRules = ParseRules(
+            "-[ ]-> k(c[])",
+            "-[ (c@1@Out(_Initial[]), a0) ]-> <a0: c@1@Out(_Write(v))>",
+            "-[ (c@2@In(_Initial[]), a0) ]-> <a0: c@2@In(_Waiting[])>",
+            "-[ (c@1@Out(_Initial[]), a0), (c@1@Out(_Write(v)), a1), (c@2@In(_Waiting[]), b0) : { a0 <@ a1 } ]-> <a1: c@1@Out(_Shut[])>, <b0: c@2@In(_Read(v))>",
+            "k(c[])(a0) -[ (c@1@Out(_Write(_vLatest)), a0) ]-> k(_vLatest)",
+            "-[ (c@2@In(_Initial[]), a0), (c@2@In(_Waiting[]), a1), (c@2@In(_Read(_v0)), a2) : { a0 <@ a1, a1 <@ a2 } ]-> <a2: c@2@In(_Shut[])>");
 
         DoTest(testSource, expectedInits, expectedRules);
+    }
+
+    private static HashSet<Rule> ParseRules(params string[] ruleSrcs)
+    {
+        RuleParser rp = new();
+        return new(from rs in ruleSrcs select rp.Parse(rs));
     }
 
     private static void DoTest(string piSource, HashSet<State> expectedStates, HashSet<Rule> expectedRules)
