9d5cd728a8003283488411f72b9285f05d2a5e4c
Nick Curran <6274048+n-crrn@users.noreply.github.com>
2022-05-03 15:05:19 +1000

    Minor code clean-up - removal of unnecessary usings and rearrangement to make it clear which using statements are out-of-project and in-project.

--- StatefulHorn/QueryEngine.cs
+++ StatefulHorn/QueryEngine.cs
@@ -1,9 +1,10 @@
-﻿using StatefulHorn.Messages;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+
+using StatefulHorn.Messages;
 
 namespace StatefulHorn;
 
