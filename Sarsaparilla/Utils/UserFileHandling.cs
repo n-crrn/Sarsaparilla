b487bf7be25abe988fd8db7d249a2ab3aa9daa20
Nick Curran <6274048+n-crrn@users.noreply.github.com>
2022-06-06 18:48:07 +1000

    Added the ability to read and write files from the Appleid Pi page. Tests pass except BobSDTestAttack.

---
+++ Sarsaparilla/Utils/UserFileHandling.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using System.Text;
+
+using Microsoft.JSInterop;
+
+namespace Sarsaparilla.Utils;
+
+public static class UserFileHandling
+{
+
+    private const string SaveFromStreamJSFunc = "saveFromStream";
+
+    private static readonly UTF8Encoding Utf8 = new();
+
+    public static async Task SaveStringToFile(IJSRuntime js, string filename, string data)
+    {
+        using MemoryStream memStream = new(Utf8.GetBytes(data));
+        using DotNetStreamReference dnStreamRef = new(memStream);
+        await js.InvokeVoidAsync(SaveFromStreamJSFunc, filename, dnStreamRef);
+    }
+
+}
