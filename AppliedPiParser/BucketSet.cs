1b6198124aa3a6727fbe944465dd7f45cfd0ce43
Nick Curran <6274048+n-crrn@users.noreply.github.com>
2022-06-26 17:15:25 +1000

    Added comments to AppliedPi.BucketSet. No functional changes made.

--- AppliedPiParser/BucketSet.cs
+++ AppliedPiParser/BucketSet.cs
@@ -6,19 +6,31 @@ using System.Linq;
 namespace AppliedPi;
 
 /// <summary>
-/// This is an Abstract Data Type designed to allow values to be allocated to a bucket based on
-/// a key. This pattern comes up multiple times in the AppliedPi library.
+/// This is an Abstract Data Type designed to allow values to be allocated to a bucket (set) based
+/// on a key. This pattern comes up multiple times in the AppliedPi library.
 /// </summary>
 public class BucketSet<TKey, TValue> : IEnumerable<(TKey, IReadOnlySet<TValue>)> where TKey : notnull 
 {
 
+    #region Constructors.
+
+    /// <summary>Create a new, empty BucketSet.</summary>
     public BucketSet() { }
 
+    /// <summary>
+    /// Create a new BucketSet with one key pointing to one bucket containing only the given value.
+    /// </summary>
+    /// <param name="k">The key.</param>
+    /// <param name="v">The value.</param>
     public BucketSet(TKey k, TValue v)
     {
         Add(k, v);
     }
 
+    /// <summary>
+    /// Create a new BucketSet using an enumerable of tuples of keys and values.
+    /// </summary>
+    /// <param name="initialItems">Enumerable of tuples.</param>
     public BucketSet(IEnumerable<(TKey, TValue)> initialItems)
     {
         foreach ((TKey k, TValue v) in initialItems)
@@ -27,6 +39,11 @@ public class BucketSet<TKey, TValue> : IEnumerable<(TKey, IReadOnlySet<TValue>)>
         }
     }
 
+    /// <summary>
+    /// Create a new BucketSet using an enumerable of tuples of keys and enumerables containing
+    /// values.
+    /// </summary>
+    /// <param name="initialBuckets">Initial values.</param>
     public BucketSet(IEnumerable<(TKey, IEnumerable<TValue>)> initialBuckets)
     {
         foreach ((TKey k, IEnumerable<TValue> vSet) in initialBuckets)
@@ -35,6 +52,11 @@ public class BucketSet<TKey, TValue> : IEnumerable<(TKey, IReadOnlySet<TValue>)>
         }
     }
 
+    /// <summary>
+    /// Create a new BucketSet containing the same values as other. All containing structures
+    /// (e.g. dictionaries and sets) are duplicated as part of the creation.
+    /// </summary>
+    /// <param name="other">The BucketSet to duplicate.</param>
     public BucketSet(BucketSet<TKey, TValue> other)
     {
         foreach ((TKey k, HashSet<TValue> v) in other._Buckets)
@@ -43,12 +65,29 @@ public class BucketSet<TKey, TValue> : IEnumerable<(TKey, IReadOnlySet<TValue>)>
         }
     }
 
+    #endregion
+    #region Properties.
+
+    /// <summary>The editable Buckets member.</summary>
     private readonly Dictionary<TKey, HashSet<TValue>> _Buckets = new();
 
+    /// <summary>
+    /// Provides public access to the Buckets list. External users are expected to not add or 
+    /// remove items from individual buckets.
+    /// </summary>
     public IReadOnlyDictionary<TKey, HashSet<TValue>> Buckets => _Buckets;
 
+    /// <summary>The total number of items held by the BucketSet in every bucket.</summary>
     public int Count => (from bv in _Buckets.Values select bv.Count).Sum();
 
+    #endregion
+    #region Adding and querying items.
+
+    /// <summary>
+    /// Add the given key and value pair to the BucketSet. A new bucket is created if necessary.
+    /// </summary>
+    /// <param name="k">Key.</param>
+    /// <param name="v">Value.</param>
     public void Add(TKey k, TValue v)
     {
         if (_Buckets.TryGetValue(k, out HashSet<TValue>? bucket))
@@ -61,6 +100,11 @@ public class BucketSet<TKey, TValue> : IEnumerable<(TKey, IReadOnlySet<TValue>)>
         }
     }
 
+    /// <summary>
+    /// Creates a union of two BucketSets. All key and value pairs of other are incorporated into
+    /// this BucketSet.
+    /// </summary>
+    /// <param name="other">BucketSet with values to copy.</param>
     public void UnionWith(BucketSet<TKey, TValue> other)
     {
         foreach ((TKey k, IEnumerable<TValue> vSet) in other)
@@ -76,14 +120,30 @@ public class BucketSet<TKey, TValue> : IEnumerable<(TKey, IReadOnlySet<TValue>)>
         }
     }
 
+    /// <summary>
+    /// Returns true if the key k is held within this BucketSet.
+    /// </summary>
+    /// <param name="k">Key.</param>
+    /// <returns>True if key held.</returns>
     public bool ContainsKey(TKey k) => _Buckets.ContainsKey(k);
 
+    /// <summary>Accesses buckets based on key.</summary>
+    /// <param name="key">Key of set to retrieve.</param>
+    /// <returns>The set indicated by key.</returns>
     public IReadOnlySet<TValue> Get(TKey key) => _Buckets[key];
 
+    /// <summary>Indexer for accessing buckets by key.</summary>
+    /// <param name="key">Key of set to retrieve.</param>
+    /// <returns>The set indicated by key.</returns>
     public IReadOnlySet<TValue> this[TKey key] => _Buckets[key];
 
+    #endregion
     #region IEnumerable implementation.
 
+    /// <summary>
+    /// A coroutine for iterating through the buckets of the BucketSet.
+    /// </summary>
+    /// <returns>Enumerator that returns a tuple consisting of the key and the value.</returns>
     private IEnumerator<(TKey, IReadOnlySet<TValue>)> InnerGetEnumerator()
     {
         foreach ((TKey k, HashSet<TValue> vSet) in _Buckets)
