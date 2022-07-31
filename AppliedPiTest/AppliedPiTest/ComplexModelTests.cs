ffbe02f89a46cc5180f787b94abbb71487273861
Nick Curran <6274048+n-crrn@users.noreply.github.com>
2022-07-31 10:52:21 +1000

    Added new complex test to test suite. Test is cucrently failing and requires further investigation.

--- AppliedPiTest/AppliedPiTest/ComplexModelTests.cs
+++ AppliedPiTest/AppliedPiTest/ComplexModelTests.cs
@@ -63,4 +63,58 @@ process
         await IntegrationTests.DoTest(piSource, true);
     }
 
+    /// <summary>
+    /// Model that should leak both bobl[] and bobr[] in the same session.
+    /// </summary>
+    /// <returns>Awaitable Task.</returns>
+    [TestMethod]
+    public async Task LeakTupleNameModelTest()
+    {
+            string piSource =
+@"type key.
+
+const left: bitstring.
+const right: bitstring.
+
+fun h(bitstring, bitstring): bitstring.
+fun pk(key): key.
+fun enc(bitstring, key): bitstring.
+reduc forall x: bitstring, y: key; dec(enc(x, pk(y)), y) = x.
+
+free publicChannel: channel.
+free bobl: bitstring [private].
+free bobr: bitstring [private].
+
+set maximumTerms = 4000.
+query attacker((bobl, bobr)).
+
+let SD(b: channel, sk: key) =
+    new mStart: bitstring;   (* State value of the security device. *)
+    in(b, x: bitstring);     (* Arbitrary value. *)
+    let mUpdated: bitstring = h(mStart, x) in
+    out(b, mUpdated);        (* Send state value, simulate read. *)
+    in(b, enc_rx: bitstring);
+    let (m_f: bitstring, s_l: bitstring, s_r: bitstring) = dec(enc_rx, sk) in
+    if m_f = h(mUpdated, left) then
+    out(b, s_l)
+    else
+    if m_f = h(mUpdated, right) then
+        out(b, s_r).
+
+let Bob(left_or_right: bitstring) =
+    new b: channel;
+    new k: key;
+    out(publicChannel, b);
+    ( SD(b, k) | 
+    ( new arb: bitstring;
+        out(b, arb);
+        in(b, readValue: bitstring);
+        out(b, enc((h(readValue, left_or_right), bobl, bobr), pk(k)));
+        in(b, v: bitstring) ) ).
+
+process Bob(left) | Bob(right) | !in(publicChannel, w: bitstring).
+";
+        await IntegrationTests.DoTest(piSource, true);
+    } 
+
 }
